941fe9d97a958675808f6aaaa6e6d047a0ebf4ec
yoontaepark <65579629+yoontaepark@users.noreply.github.com>
2021-07-26 02:04:45 +0900

    Update 2) 344. reverse string.py

--- Leetcode/2) 344. reverse string.py
+++ Leetcode/2) 344. reverse string.py
@@ -1,6 +1,5 @@
-# Write a function that reverses a string. The input string is given as an array of characters s.
-
- 
+# 2) 344. reverse string
+# Write a function that reverses a string. The input string is given as an array of characters s. 
 
 # Example 1:
 
