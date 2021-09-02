a2d502802b96862074c1ff77a9f6f11656cd13a2
yoontaepark <65579629+yoontaepark@users.noreply.github.com>
2021-09-01 21:46:21 -0400

    Update leetcode_all.py

--- leetcode_all.py
+++ leetcode_all.py
@@ -273,3 +273,56 @@ class Solution:
         
         return reverse(head)
 
+
+
+# 2. Add Two Numbers: 순서대로 정의하면서 풀어라
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+        
+class Solution:      
+    # linked list -> reverse linked list
+    def reverseList(self, head):
+        node, prev = head, None
+            
+        while node:
+            next, node.next = node.next, prev
+            prev, node = node, next            
+            
+        return prev
+        
+        # linked list -> python list 
+    def toList(self, node):
+        list = []
+        while node:
+            list.append(node.val)
+            node = node.next
+                
+        return list
+        
+        
+    # python list -> reversed linked list
+    def toReversedLinkedList(self, result):
+        prev = None
+        for r in result:
+            node = ListNode(r)
+            node.next = prev
+            prev = node
+            
+        return node       
+        
+        
+        # add two linked list 
+    def addTwoNumbers(self, l1, l2):
+        a = self.toList(self.reverseList(l1))
+        b = self.toList(self.reverseList(l2))
+            
+        resultStr = int(''.join(str(e) for e in a)) + \
+                    int(''.join(str(e) for e in b))
+            
+        return self.toReversedLinkedList(str(resultStr))       
+        
+        
+        
