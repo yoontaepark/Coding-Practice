f711c34e420abdb5d8c598c15a8f7ae797634be5
yoontaepark <ytpark540@gmail.com>
2023-01-31 20:04:32 -0500

    update_0131

--- leetcode/7. Tree.py
+++ leetcode/7. Tree.py
@@ -8,7 +8,40 @@ from typing import List, Optional
 import collections
 
 # - 문제
-#     - (Medium) Leetcode 743. Network Delay Time: (https://leetcode.com/problems/network-delay-time/)
-#     - 풀이: 최단경로찾기) bfs (or 다익스트라) 구현한다고 생각하면서 접근 
+# 104. Maximum Depth of Binary Tree: https://leetcode.com/problems/maximum-depth-of-binary-tree/description/
 
-            
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        # idea: define depth, and iterate to see any child nodes for each nodes
+        if not root:
+            return 0
+        
+        # define a deque that includes entire tree as a list 
+        # also, define depth so that we can return depth as a result
+        dequeue = collections.deque([root])
+        depth = 0
+
+        # if there is any node available at that stage, iterate 
+        while dequeue:
+            # depth + 1 if that stage is available 
+            depth += 1
+
+            # iterate each roots to add childs to dequeue
+            for _ in range(len(dequeue)): 
+                # popleft so that we can check from old ones
+                cur_root = dequeue.popleft()
+                # check if there are any left/right child and if so, append 
+                if cur_root.left:
+                    dequeue.append(cur_root.left)
+                # same applies to right child 
+                if cur_root.right:
+                    dequeue.append(cur_root.right)
+
+        # returning final result
+        return depth            
